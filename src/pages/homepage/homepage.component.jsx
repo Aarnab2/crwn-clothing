662a1db4b8bb08ceda2e9c042fe9a8bf678890c4
Arnab Das <adas@aapnainfotech.com>
2021-05-26 19:57:34 +0530

    added

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.component'
+//import MenuItem from '../../components/menu-item/menu-item.component'
+const HomePage = () => {
+    return <div className='homepage'>
+    <Directory>
+    </Directory>
+       </div>
+}
+
+export default HomePage;
