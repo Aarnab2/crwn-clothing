3902ae4d6d53bcf9e515ec836f5a893b988d58b9
Arnab Das <adas@aapnainfotech.com>
2021-05-30 14:08:59 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom' 
+
+const HatsPage = (props)=>{
+  console.log(props)
+  return <div>
+    <h1>HATS PAGE</h1>
+  </div>
+}
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      
+      <Route  exact path='/' component={HomePage}></Route>
+      <Route exact path='/shop/hats' component={HatsPage}></Route>
+      
+      
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 //import MenuItem from '../../components/menu-item/menu-item.component'
-const HomePage = () => {
+const HomePage = (props) => {
+    console.log(props)
     return <div className='homepage'>
     <Directory>
     </Directory>
