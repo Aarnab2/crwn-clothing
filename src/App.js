61eaa30b46c1fe99997c8db96063c3c8b0f200fe
Arnab Das <adas@aapnainfotech.com>
2021-05-31 17:04:55 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import { Route } from 'react-router-dom' 
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 // const HatsPage = (props)=>{
 //   console.log(props)
@@ -13,11 +14,11 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
-      
-      <Route  exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop' component={ShopPage}></Route>
-      
-      
+      <Header></Header>
+      <switch><Route  exact path='/' component={HomePage}></Route>
+      <Route  path='/shop' component={ShopPage}></Route>
+      </switch>
+
     </div>
   );
 }
