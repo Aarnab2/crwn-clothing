773f88e236a40a936465d2b01a4bb023ac2b42bc
Arnab Das <adas@aapnainfotech.com>
2021-06-15 14:14:48 +0530

    added

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -5,3 +5,10 @@ export const toggelCartHidden = ()=>{
     type: CartActionTypes.TOGGLE_CART_HIDDEN
   }  
 }
+
+export const addItem = (item)=>{
+    return {
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+    }
+}
